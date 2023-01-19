4b1e14056b9200845859ffb51f56e319d454ba5f
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-19 19:46:00 -0300

    Adição das interfaces repository

--- src/main/java/com/nicolas/model/entity/Lanche.java
+++ src/main/java/com/nicolas/model/entity/Lanche.java
@@ -13,7 +13,7 @@ public class Lanche {
     public String getNome() { return nome; }
 
     public void setNome(String nome) { this.nome = nome; }
-    
+
     public Double getPreco() { return preco; }
 
     public void setPreco(Double preco) { this.preco = preco; }
---
+++ src/main/java/com/nicolas/repository/LancheRepository.java
@@ -0,0 +1,3 @@
+package com.nicolas.repository;
+
+public interface LancheRepository {}
