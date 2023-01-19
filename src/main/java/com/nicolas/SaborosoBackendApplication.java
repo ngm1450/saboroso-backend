cc3704b5d7b4475a495b1d7e1c3f35561b3e5ec8
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-19 19:26:23 -0300

    Criação do projeto utilizando Spring Initializr

---
+++ src/main/java/com/nicolas/SaborosoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborosoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborosoBackendApplication.class, args);
+	}
+
+}
