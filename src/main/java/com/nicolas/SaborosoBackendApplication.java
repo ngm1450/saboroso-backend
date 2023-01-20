ea977752f043ef826eb8c15caa2f51e2fe24fce9
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-20 14:04:09 -0300

    #3 melhorias metodo main

--- src/main/java/com/nicolas/SaborosoBackendApplication.java
+++ src/main/java/com/nicolas/SaborosoBackendApplication.java
@@ -1,17 +1,51 @@
 package com.nicolas;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SaborosoBackendApplication {
 
+	private static final Logger LOG = LogManager.getLogger(SaborosoBackendApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SaborosoBackendApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(SaborosoBackendApplication.class, args);
+
+		try {
+			Environment env = ctx.getEnvironment();
+			LOG.info("\n\n *** \n\n"
+							+ "\tSABOROSO - BACKEND iniciado com sucesso!\n"
+							+ "\tDisponivel nos enderecos:\n"
+							+ "\tLocal: http://localhost:{}{}\n"
+							+ "\tSwagger Url: http://localhost:{}{}/swagger-ui.html\n"
+							+ "\tActuator: http://localhost:{}{}/actuator\n"
+							+ "\n *** \n\n",
+					env.getProperty("server.port"),
+					env.getProperty("server.servlet.context-path"),
+
+					InetAddress.getLocalHost().getHostAddress(),
+					env.getProperty("server.port"),
+					env.getProperty("server.servlet.context-path"),
+
+					env.getProperty("server.port"),
+					env.getProperty("server.servlet.context-path"));
+
+		} catch (UnknownHostException e) {
+			LOG.error("Falha ao executar aplicacao: {}", e);
+			ctx.close();
+		}
 	}
 
 }
