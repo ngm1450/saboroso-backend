1b2f1c34eb252f235ee6ff948ea8ac055a000307
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-22 19:29:31 -0300

    #5 limpeza e escrita do README

--- src/main/java/com/nicolas/SaborosoBackendApplication.java
+++ src/main/java/com/nicolas/SaborosoBackendApplication.java
@@ -4,15 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SaborosoBackendApplication {
@@ -21,7 +12,13 @@ public class SaborosoBackendApplication {
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext ctx = SpringApplication.run(SaborosoBackendApplication.class, args);
+		SpringApplication.run(SaborosoBackendApplication.class, args);
+
+		LOG.info("\n\n *** \n\n"
+				+ "\tSABOROSO - BACKEND iniciado com sucesso!\n"
+				+ "\tDisponivel no endereco:\n"
+				+ "\tSwagger Url: http://localhost:8080/swagger-ui/index.html\n"
+				+ "\n *** \n\n");
 
 	}
 
